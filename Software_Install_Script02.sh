b35dbc0b1423ca55501b61d4a93b8444bc34022e
gregoris-io <71711222+gregoris-io@users.noreply.github.com>
2022-05-16 13:51:31 -0400

    Add files via upload

---
+++ Software_Install_Script02.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+## System Software Install Script, 
+
+## Gnome-Boxes, GNUSim8085, EMACs,  wine and Lutris can be sourced from the offical ubuntu repo.
+
+## Brave
+flatpak install com.brave.Browser
+ 
+## MS Edge
+flatpak install flathub com.microsoft.Edge
+
+## Joplin
+flatpak install c.joplin_desktop
+
+## OBS Studio
+flatpak install com.obsproject.Studio
+
+## Steam
+flatpak install com.valvesoftware.Steam
+
+## QT Creator
+flatpak install flathub io.qt.QtCreator
+
+## Intellij Ultimate
+flatpak install flathub ins.IntelliJ-IDEA-Ultimate
+
+## Andorid Studio
+com.google.AndroidStudio
+
+##Instal CLION
+flatpak install flathub com.jetbrains.CLion
+
+## INstall VSCodium
+flatpak install com.vscodium.codium
+
+## Visual Studio
+flatpak install com.visualstudio.code
+
+##Install Zoom
+flatpak install flathub us.zoom.Zoom
