ef44a7eb019ba39cbe5b32303b1e8e08e99baa64
gregoris-io <71711222+gregoris-io@users.noreply.github.com>
2022-06-09 23:16:14 -0400

    Update Software_Install_Script02.sh

--- Software_Install_Script02.sh
+++ Software_Install_Script02.sh
@@ -5,37 +5,37 @@
 ## Gnome-Boxes, GNUSim8085, EMACs,  wine and Lutris can be sourced from the offical ubuntu repo.
 
 ## Brave
-flatpak install com.brave.Browser
+flatpak install com.brave.Browser -y
  
 ## MS Edge
-flatpak install flathub com.microsoft.Edge
+flatpak install flathub com.microsoft.Edge -y
 
 ## Joplin
-flatpak install c.joplin_desktop
+flatpak install c.joplin_desktop -y
 
 ## OBS Studio
-flatpak install com.obsproject.Studio
+flatpak install com.obsproject.Studio -y
 
 ## Steam
-flatpak install com.valvesoftware.Steam
+flatpak install com.valvesoftware.Steam -y
 
 ## QT Creator
-flatpak install flathub io.qt.QtCreator
+flatpak install flathub io.qt.QtCreator -y
 
 ## Intellij Ultimate
-flatpak install flathub ins.IntelliJ-IDEA-Ultimate
+flatpak install flathub ins.IntelliJ-IDEA-Ultimate -y
 
 ## Andorid Studio
-com.google.AndroidStudio
+com.google.AndroidStudio -y
 
 ##Instal CLION
-flatpak install flathub com.jetbrains.CLion
+flatpak install flathub com.jetbrains.CLion -y
 
 ## INstall VSCodium
-flatpak install com.vscodium.codium
+flatpak install com.vscodium.codium -y
 
 ## Visual Studio
-flatpak install com.visualstudio.code
+flatpak install com.visualstudio.code -y
 
 ##Install Zoom
-flatpak install flathub us.zoom.Zoom
+flatpak install flathub us.zoom.Zoom -y
